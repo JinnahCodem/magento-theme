62ad2bd5e1b922fb104dad0306c4623e8ca150fc
JinnahCodem <mohamedali.jinnah@codem.com>
2023-02-14 00:38:07 +0530

    Responsive design done for Catalog and Cart Page

--- app/code/Codem/CategorySide/Block/Categorylist.php
+++ app/code/Codem/CategorySide/Block/Categorylist.php
@@ -1,22 +1,58 @@
-<?php
+<?php declare(strict_types=1);
+
+/*
+ * Copyright (c) 2022 ReCodem Pvt Ltd All rights reserved
+ */
 
 namespace Codem\CategorySide\Block;
 
-class Categorylist extends \Magento\Framework\View\Element\Template
+use Magento\Catalog\Model\Category;
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Catalog\Model\CategoryRepository;
+use Magento\Catalog\Model\ResourceModel\Category\Collection;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Class Category List to get the category name
+ */
+class Categorylist extends Template
 {
+    /**
+     * @var CollectionFactory
+     */
     protected $_categoryFactory;
 
+    /**
+     * @var StoreManagerInterface
+     */
     protected $_storeManager;
 
+    /**
+     * @var CategoryFactory
+     */
     protected $_categoryNameFactory;
 
+    /**
+     * @param Context $context
+     * @param CategoryFactory $categoryNameFactory
+     * @param CollectionFactory $collecionFactory
+     * @param StoreManagerInterface $storeManager
+     * @param Registry $registry
+     * @param CategoryRepository $categoryRepository
+     * @param array $data
+     */
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Catalog\Model\CategoryFactory $categoryNameFactory,
-        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collecionFactory,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\Registry $registry,
-        \Magento\Catalog\Model\CategoryRepository $categoryRepository,
+        Context $context,
+        CategoryFactory $categoryNameFactory,
+        CollectionFactory $collecionFactory,
+        StoreManagerInterface $storeManager,
+        Registry $registry,
+        CategoryRepository $categoryRepository,
         array $data = []
     ) {
         $this->_coreRegistry = $registry;
@@ -27,6 +63,10 @@ class Categorylist extends \Magento\Framework\View\Element\Template
         parent::__construct($context, $data);
     }
 
+    /**
+     * @return int
+     * @throws NoSuchEntityException
+     */
     public function getEnableCategory()
     {
         $storeGroupId = $this->_storeManager->getStore()->getStoreGroupId();
@@ -36,6 +76,11 @@ class Categorylist extends \Magento\Framework\View\Element\Template
         return $rootCategoryId;
     }
 
+    /**
+     * @param $categoryId
+     * @return Category[]|Collection
+     * @throws NoSuchEntityException
+     */
     public function getRootSubCategory($categoryId)
     {
         $category = $this->categoryRepository->get($categoryId);
@@ -43,6 +88,11 @@ class Categorylist extends \Magento\Framework\View\Element\Template
         return $category->getChildrenCategories();
     }
 
+    /**
+     * @param $categoryId
+     * @return mixed
+     * @throws NoSuchEntityException
+     */
     public function getCategoryName($categoryId)
     {
         $category = $this->_categoryNameFactory->create()->load($categoryId)->setStore($this->_storeManager->getStore());
